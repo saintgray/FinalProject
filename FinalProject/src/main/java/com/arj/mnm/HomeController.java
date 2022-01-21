eaeff710380581bc7f647f6177dbc55955fa0f6e
OJH <saintgray0@gmail.com>
2022-01-21 20:50:11 +0900

    conflict test

--- FinalProject/src/main/java/com/arj/mnm/HomeController.java
+++ FinalProject/src/main/java/com/arj/mnm/HomeController.java
@@ -24,19 +24,8 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";//지원
-		// 오현
-		//추가
-		//ㅇㄹㅇㄹ
+		//conflict test
+		return null;
 	}
 	
 }
