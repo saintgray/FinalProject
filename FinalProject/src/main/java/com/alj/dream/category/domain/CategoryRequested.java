7d277974bb3e22af93c07f3383618f67f2aee7fe
OJH <saintgray0@gmail.com>
2022-01-30 23:14:39 +0900

    카테고리 도메인 객체 생성

---
+++ FinalProject/src/main/java/com/alj/dream/category/domain/CategoryRequested.java
@@ -0,0 +1,28 @@
+package com.alj.dream.category.domain;
+
+import java.util.List;
+
+public class CategoryRequested {
+	
+	private List<String> interest;
+	
+	public CategoryRequested() {
+		
+	}
+
+	public CategoryRequested(List<String> interest) {
+		
+		this.interest = interest;
+	}
+
+	public List<String> getInterest() {
+		return interest;
+	}
+
+	public void setInterest(List<String> interest) {
+		this.interest = interest;
+	}
+	
+	
+
+}
