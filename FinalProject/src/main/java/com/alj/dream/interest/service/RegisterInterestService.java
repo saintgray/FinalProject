b923c7b58b9a348d4d4e99129598f4048f1a6cf9
OJH <saintgray0@gmail.com>
2022-02-01 02:27:14 +0900

    새로 가입한 회원의 관심사를 DB 테이블에 추가하는 RegisterInterestService

---
+++ FinalProject/src/main/java/com/alj/dream/interest/service/RegisterInterestService.java
@@ -0,0 +1,39 @@
+package com.alj.dream.interest.service;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alj.dream.interest.dao.InterestDao;
+import com.alj.dream.interest.domain.Interest;
+import com.alj.dream.member.domain.RegisterInfo;
+
+@Service
+public class RegisterInterestService {
+
+	
+	private SqlSessionTemplate sst;
+	
+	public RegisterInterestService() {
+	
+	}
+	@Autowired
+	public RegisterInterestService(SqlSessionTemplate sst) {
+	
+		this.sst = sst;
+	}
+	
+	
+	public int insertInterest(RegisterInfo infos) {
+		
+		for(String cat_idx: infos.getInterest()) {
+			sst.getMapper(InterestDao.class).registerUserInterest(new Interest(String.valueOf(infos.getM_idx()), cat_idx));
+		}
+		
+		
+		return 0;
+		
+	}
+	
+	
+}
