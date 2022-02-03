5f7e526be0ea8243795661893f2163e497f3306b
chuckledji <92347598+chuckledji@users.noreply.github.com>
2022-02-03 16:06:45 +0900

    request관련 domain

---
+++ FinalProject/src/main/java/com/alj/dream/post/domain/RequestMember.java
@@ -0,0 +1,94 @@
+package com.alj.dream.post.domain;
+
+public class RequestMember {
+
+	// 게시물에 문의를 한 회원의 정보
+	
+	private int match_idx;		// 매칭 고유번호(=채팅방 고유번호)
+	private int post_idx;		// 게시물 고유번호
+	private int m_idx;			// 회원 고유번호
+	private String m_nm;		// 회원 이름
+	private String m_photo;		// 회원 사진
+	private String matched_yn;	// 매칭 여부
+	private String line;		// 회원 프로필 한줄소개
+	
+	public RequestMember() {}
+	
+	public RequestMember(int match_idx, int post_idx, int m_idx, String m_nm, String m_photo, String matched_yn,
+			String line) {
+		this.match_idx = match_idx;
+		this.post_idx = post_idx;
+		this.m_idx = m_idx;
+		this.m_nm = m_nm;
+		this.m_photo = m_photo;
+		this.matched_yn = matched_yn;
+		this.line = line;
+	}
+	
+	
+
+	public int getMatch_idx() {
+		return match_idx;
+	}
+
+	public void setMatch_idx(int match_idx) {
+		this.match_idx = match_idx;
+	}
+
+	public int getPost_idx() {
+		return post_idx;
+	}
+
+	public void setPost_idx(int post_idx) {
+		this.post_idx = post_idx;
+	}
+
+	public int getM_idx() {
+		return m_idx;
+	}
+
+	public void setM_idx(int m_idx) {
+		this.m_idx = m_idx;
+	}
+
+	public String getM_nm() {
+		return m_nm;
+	}
+
+	public void setM_nm(String m_nm) {
+		this.m_nm = m_nm;
+	}
+
+	public String getM_photo() {
+		return m_photo;
+	}
+
+	public void setM_photo(String m_photo) {
+		this.m_photo = m_photo;
+	}
+
+	public String getMatched_yn() {
+		return matched_yn;
+	}
+
+	public void setMatched_yn(String matched_yn) {
+		this.matched_yn = matched_yn;
+	}
+
+	public String getLine() {
+		return line;
+	}
+
+	public void setLine(String line) {
+		this.line = line;
+	}
+
+	@Override
+	public String toString() {
+		return "RequestingMember [match_idx=" + match_idx + ", post_idx=" + post_idx + ", m_idx=" + m_idx + ", m_nm="
+				+ m_nm + ", m_photo=" + m_photo + ", matched_yn=" + matched_yn + ", line=" + line + "]";
+	}
+	
+	
+	
+}
