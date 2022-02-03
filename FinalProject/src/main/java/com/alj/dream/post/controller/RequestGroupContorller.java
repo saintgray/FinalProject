3dd3e3cf4d59f82cd622f4d1c97bc44f17602c8f
chuckledji <92347598+chuckledji@users.noreply.github.com>
2022-02-03 16:07:07 +0900

    request관련 controller와service

---
+++ FinalProject/src/main/java/com/alj/dream/post/controller/RequestGroupContorller.java
@@ -0,0 +1,26 @@
+package com.alj.dream.post.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.alj.dream.post.service.RequestGroupService;
+
+@Controller
+public class RequestGroupContorller {
+
+	@Autowired
+	private RequestGroupService groupService;
+	//받은 요청 페이지에 들어갈 그룹을 가져온다.
+	
+	@RequestMapping("/post/requestgroup")
+	public void getGroupPage(HttpSession session,									// session에 현재 사용자 idx와 m_type을가져온다
+							Model model) {
+		model.addAttribute("requestGroup", groupService.getRequestGroup(session));	// 그룹 리스트부분
+	}
+		
+	
+}
