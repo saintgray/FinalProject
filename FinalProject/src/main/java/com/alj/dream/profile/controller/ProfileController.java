a5492ca368edbfdba400717e692bf94a472a6698
OJH <saintgray0@gmail.com>
2022-02-03 17:28:04 +0900

    실수로 master 에서 작업했습니다...

--- FinalProject/src/main/java/com/alj/dream/profile/controller/ProfileController.java
+++ FinalProject/src/main/java/com/alj/dream/profile/controller/ProfileController.java
@@ -1,16 +1,33 @@
 package com.alj.dream.profile.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.alj.dream.review.domain.AverageStarService;
+
+import security.AccountDetails;
+
 @Controller
 @RequestMapping("/member/profile")
 public class ProfileController {
+	
+	private AverageStarService avgService;
+	public ProfileController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Autowired
+	public ProfileController(AverageStarService avgService) {
+		this.avgService = avgService;
+	}
+
 
 	@GetMapping("/main")
-	public String showProfileMain() {
-
+	public String showProfileMain(Authentication authentication) {
+		AccountDetails logininfo= (AccountDetails)authentication.getPrincipal();
 		return "member/profilemain";
 	}
 
--- FinalProject/src/main/java/com/alj/dream/review/dao/ReviewDao.java
+++ FinalProject/src/main/java/com/alj/dream/review/dao/ReviewDao.java
@@ -1,5 +1,7 @@
 package com.alj.dream.review.dao;
 
 public interface ReviewDao {
+	
+	String getStars(String m_idx);
 
 }
