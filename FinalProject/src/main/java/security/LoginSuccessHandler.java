fb2a60bbe253e5967695369ecc5aa35fdf16d9aa
OJH <saintgray0@gmail.com>
2022-02-03 11:56:19 +0900

    로그인 성공시 관리자는 관리자 메인페이지로, 일반 회원은 메인 화면으로 보냄
    AuthenticationSuccessHandler 인터페이스를 구현하여
    
    onAuthenticationSuccess 메소드를 오버라이딩

--- FinalProject/src/main/java/security/LoginSuccessHandler.java
+++ FinalProject/src/main/java/security/LoginSuccessHandler.java
@@ -1,5 +1,47 @@
 package security;
 
-public class LoginSuccessHandler {
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+
+public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+	
+	
+	// AuthenticationSuccessHandler 인터페이스를 구현한 LoginSuccessHandler 클래스
+	// 로그인에 성공시 성공한 허가증에 있는 GrantedAuthority 콜렉션에 ROLE_ADMIN 이라는 권한이 있다면
+	// 바로 관리자 페이지로 이동하며, 그렇지 않다면 알려드림 서비스의 메인 페이지로 이동한다.
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		AccountDetails logininfo= (AccountDetails)	authentication.getPrincipal();
+		
+		Iterator<GrantedAuthority> itr= logininfo.getAuthorities().iterator();
+		String redirectUrl="/";
+		
+		
+		
+		while(itr.hasNext()) {
+			if(itr.next().getAuthority().equals("ROLE_ADMIN")) {
+				redirectUrl="/admin/manage";
+				break;
+			}
+		}
+		
+		
+		response.sendRedirect(request.getContextPath().concat(redirectUrl));
+		
+	}
+	
+	
+	
 
 }
--- FinalProject/src/main/java/security/LogoutSuccessHandler.java
+++ FinalProject/src/main/java/security/LogoutSuccessHandler.java
@@ -1,5 +1,6 @@
 package security;
 
-public class LogoutSuccessHandler {
+// 로그아웃 성공시 처리해야할 작업이 있다면 LogoutSuccessHandler 인터페이스를 구현하여 이 클래스 내에서 처리하도록 한다.
+public class LogoutSuccessHandler  {
 
 }
