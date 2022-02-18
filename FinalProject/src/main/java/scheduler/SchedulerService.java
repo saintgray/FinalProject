d7e762bf636a11cb5958c7642a4d6b7d659ca6d1
OJH <saintgray0@gmail.com>
2022-02-18 16:29:57 +0900

    협업을 위한 각자의 파일 저장 경로 ServerPath 클래스에 할당

--- FinalProject/src/main/java/scheduler/SchedulerService.java
+++ FinalProject/src/main/java/scheduler/SchedulerService.java
@@ -15,24 +15,32 @@ import com.alj.dream.file_profile.dao.ProfileFilesDao;
 @EnableScheduling
 public class SchedulerService{
 	
-	private static final String PROFILE_FILES_PATH="C:\\Users\\OJH\\Documents\\GitHub\\FinalProject\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\mnm\\resources\\files\\member\\profile_attachfiles";
+	
 	
 	private SqlSessionTemplate sst;
+	private ServerPath serverPath;
+	
 	
 	public SchedulerService() {
 
 	}
 	
 	
+	
 	@Autowired
-	public SchedulerService(SqlSessionTemplate sst) {
+	public SchedulerService(SqlSessionTemplate sst, ServerPath serverPath) {
 		this.sst = sst;
+		this.serverPath = serverPath;
 	}
+
+
 
 
 	// 매일 새벽 2시에 실행되는 메소드
 	@Scheduled(cron = "0 0 2 * * *")
 	public void deleteInvalidFiles() {
+		
+		
 		// DB에는 삭제일이 업데이트 되었으나 파일을 삭제하는 과정에서 오류가 발생하여 미처 삭제되지 않은 파일을 일괄 삭제한다.
 		
 		// 1. file_profile 테이블에서 file_deldate 가 null 이 아닌 파일(삭제된 파일 데이터)을 모두 가져온다.
@@ -41,7 +49,7 @@ public class SchedulerService{
 		
 		// filenm=PK
 		for(String filenm : list) {
-			File file=new File(PROFILE_FILES_PATH,filenm);
+			File file=new File(serverPath.profileFilesPath,filenm);
 			System.out.println(file.exists());
 			// 삭제처리
 			
---
+++ FinalProject/src/main/java/scheduler/ServerPath.java
@@ -0,0 +1,31 @@
+package scheduler;
+
+public class ServerPath {
+	
+	public String profileFilesPath;
+	public String postFilesPath;
+	public String noticeFilesPath;
+	public String chatFilesPath;
+	
+	
+	
+	public ServerPath() {
+	
+	}
+
+
+
+	public ServerPath(String profileFilesPath, String postFilesPath, String noticeFilesPath, String chatFilesPath) {
+		this.profileFilesPath = profileFilesPath;
+		this.postFilesPath = postFilesPath;
+		this.noticeFilesPath = noticeFilesPath;
+		this.chatFilesPath = chatFilesPath;
+	}
+	
+	
+
+
+	
+	
+
+}
