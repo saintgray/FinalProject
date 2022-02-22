44ba5d6f900a1b442cc3ec54edd16450d342c1ff
jhjhkim <jihyun.kim.cd@gmail.com>
2022-02-22 18:25:51 +0900

    게시글 삭제시 db에서 첨부파일 삭제

--- FinalProject/src/main/java/com/alj/dream/post/controller/PostDeleteController.java
+++ FinalProject/src/main/java/com/alj/dream/post/controller/PostDeleteController.java
@@ -1,11 +1,16 @@
 package com.alj.dream.post.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alj.dream.file_post.domain.PostFileRequest;
 import com.alj.dream.file_post.service.PostFileDeleteService;
 import com.alj.dream.post.service.PostDeleteService;
 
@@ -21,16 +26,30 @@ public class PostDeleteController {
 	
 	@PostMapping
 	@ResponseBody
-	public int deletePost(int post_idx) {
+	public int deletePost(HttpServletRequest request, int post_idx) {
+		
+		// 게시글에 deldate 추가
+		int resultCnt = service.deletePost(post_idx);
 		
 		// 게시물 첨부파일 존재유무 확인 후 있으면 file_deldate 추가하기
-		int file_yn = fileService.getCountFile(post_idx);
+		int file_count = fileService.getCountFile(post_idx);
 		
-		if(file_yn > 0) {
-			fileService.deleteFile(post_idx);
+		if(file_count > 0) {
+			
+			// 게시물 첨부파일 삭제 메소드
+			// 파일 저장 경로
+			String saveDir = request.getSession().getServletContext().getRealPath("/resources/files/post/attachfiles");
+			List<PostFileRequest> fileList = fileService.selectAllFilesByPostIdx(post_idx);
+			
+			// deldate 추가
+			fileService.addDeldate(post_idx);
+			
+			// 실제 파일 삭제
+			fileService.deleteFiles(saveDir, fileList);
+			
 		}
 		
-		return service.deletePost(post_idx);
+		return resultCnt;
 		
 	}
 	
