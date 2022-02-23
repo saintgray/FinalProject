3e5b86865e0ec67c633b7f3c061efb6f95b4c8e6
chuckledji <92347598+chuckledji@users.noreply.github.com>
2022-02-23 22:25:02 +0900

    신고

---
+++ FinalProject/src/main/java/com/alj/dream/report/service/ReportService.java
@@ -0,0 +1,50 @@
+package com.alj.dream.report.service;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alj.dream.report.dao.ReportDao;
+import com.alj.dream.report.domain.Report;
+
+@Service
+public class ReportService {
+
+	private ReportDao dao;
+	
+	@Autowired 
+	private SqlSessionTemplate template;
+	
+	// 하루 신고횟수 확인
+	public int chkReport(int myidx, int matchidx) {
+
+		dao = template.getMapper(ReportDao.class);
+		
+		int todayRprt = dao.todayReportByMyidx(myidx, matchidx);	
+		int resultCnt = 0;
+		
+		// 오늘 이회원의 신고횟수가 하루에 다섯번 이상이라면 더이상의 신고는 불가능하다.
+		
+		if (todayRprt <6){ 
+			resultCnt = 1;  //신고해도 괜찮은사람
+		}else{
+			resultCnt = 0;   //신고해도 안괜찮은 사람 
+		}
+ 
+		
+		return resultCnt;
+	}
+	
+	
+	// 신고 등록할 때
+		public int insertReport(Report r) {
+			
+			dao = template.getMapper(ReportDao.class);
+			
+			int resultCnt = dao.insertReport(r);
+			
+			return resultCnt;
+			
+		}
+
+}
---
+++ FinalProject/src/main/java/com/alj/dream/review/controller/ReviewController.java
@@ -0,0 +1,32 @@
+package com.alj.dream.review.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alj.dream.review.service.ReviewService;
+
+@Controller
+public class ReviewController {
+	
+	@Autowired
+	private ReviewService service;
+	
+	
+	// 리뷰 등록할때
+	@RequestMapping(value="/review/regreview", method=RequestMethod.POST)
+	@ResponseBody
+	public int regReview(
+			@RequestParam("rating") int rating,
+			@RequestParam("comment") String comment,
+			@RequestParam("matchidx") int matchidx
+			) {
+		
+		int resultCnt = service.regReview(rating,comment,matchidx);
+		
+		return resultCnt;
+	}
+}
